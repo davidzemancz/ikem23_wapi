394a510e06374d5b73ff7a9e9c40b0513a493156
David Zeman <david.zeman@outlook.com>
2023-11-24 21:34:41 +0100

    cols

--- ikem23_wapi/Models/ImportTemplate.cs
+++ ikem23_wapi/Models/ImportTemplate.cs
@@ -5,9 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-
-        public int sheetNum { get; set; }
-
+        public int SheetNumber { get; set; }
         public List<ColumnDefinition> ColumnMapping { get; } = new List<ColumnDefinition>();
     }
 
