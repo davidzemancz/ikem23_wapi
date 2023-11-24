a94c11e173511cc8b2f11f3e06881b85e1896e6f
kwimbik <kwimbiks@gmail.com>
2023-11-24 19:55:28 +0100

    search templates

---
+++ test/Program.cs
@@ -0,0 +1,17 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using ikem23_wapi;
+
+namespace test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string filepath = "test.xlsx";
+            ImportTemplate it = new ImportTemplate();
+            it.ColumnMapping.Add(new ColumnDefinition {ExcelColumn = "A", PropertyName = "IdBiosepse" });
+            var a = ExcelReader.ReadExcelFile(filepath, it);
+            Console.WriteLine("");
+        }
+    }
+}
