e5edf6ba7f82dc0e721ae4fc078fc4f663d993d5
David Zeman <david.zeman@outlook.com>
2023-11-24 19:57:25 +0100

    data service

---
+++ ikem23_wapi/Controllers/ImportTemplateController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ikem23_wapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImportTemplateController : ControllerBase
+    {
+        // GET: api/<ImportTemplateController>
+        [HttpGet]
+        public IEnumerable<ImportTemplate> Get()
+        {
+            return new ImportTemplate[] { "value1", "value2" };
+        }
+
+        // GET api/<ImportTemplateController>/5
+        [HttpGet("{id}")]
+        public ImportTemplate Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ImportTemplateController>
+        [HttpPost]
+        public void Post([FromBody] ImportTemplate value)
+        {
+        }
+
+        // PUT api/<ImportTemplateController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] ImportTemplate value)
+        {
+        }
+
+        // DELETE api/<ImportTemplateController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ikem23_wapi/Services/ImportTemplateDataService.cs
@@ -0,0 +1,10 @@
+﻿namespace ikem23_wapi.Services
+{
+    public class ImportTemplateDataService
+    {
+        public async Task<List<ImportTemplate>> Get()
+        {
+            return new List<ImportTemplate>();
+        } 
+    }
+}
