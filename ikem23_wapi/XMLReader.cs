0e1683df73eec8f64c5f59430bfdd3c260d5af0f
kwimbik <kwimbiks@gmail.com>
2023-11-24 18:33:15 +0100

    upraven load

--- ikem23_wapi/XMLReader.cs
+++ ikem23_wapi/XMLReader.cs
@@ -7,9 +7,9 @@ namespace ikem23_wapi
 
     public class XMLReader
     {
-        public static ImportObj ReadEXcelFile(string fileName, ImportTemplate template)
+        public static List<PatientRecord> ReadEXcelFile(string fileName, ImportTemplate template)
         {
-            var importObj = new ImportObj();
+            var importObj = new List<PatientRecord>();
             var workbook = new XLWorkbook(fileName);
             Dictionary<int, string> colIdName = new Dictionary<int, string>();
             var ws1 = workbook.Worksheet(1);
@@ -24,16 +24,22 @@ namespace ikem23_wapi
             foreach (var row in rows)
             {
                 var rowNumber = row.RowNumber();
+                var PatientRecord = new PatientRecord { };
                 foreach (var col in ws1.ColumnsUsed())
                 {
                     int colNum = col.ColumnNumber();
                     string value  = row.Cell(colNum).Value.ToString();
-                    ColumnDefinition cd = new ColumnDefinition { Id = colNum, ExcelColumn = colIdName[colNum] };
-                    importObj.data.Add((rowNumber, cd), value);
+                    var colDef = template.ColumnMapping.Find(x => x.ExcelColumn == col.ColumnLetter());
+                    if (colDef == null) continue;
                 }
             }
 
             return importObj;
+        }
+
+        public void renameColumns(ImportTemplate template, Dictionary<int, string> colIdName)
+        {
+
         }
     }
 }
