e0ea55625f5bbb0e07b0c92fef1cd1a515816077
kwimbik <kwimbiks@gmail.com>
2023-11-24 20:57:23 +0100

    test template

--- test/Program.cs
+++ test/Program.cs
@@ -8,10 +8,21 @@ namespace test
         public static void Main(string[] args)
         {
             string filepath = "test.xlsx";
-            ImportTemplate it = new ImportTemplate();
-            it.ColumnMapping.Add(new ColumnDefinition {ExcelColumn = "A", PropertyName = "IdBiosepse" });
+            ImportTemplate it = loadTestTemplate();
             var a = ExcelReader.ReadExcelFile(filepath, it);
             Console.WriteLine("");
+        }
+
+        public static ImportTemplate loadTestTemplate()
+        {
+            ImportTemplate it = new ImportTemplate();
+            it.ColumnMapping.Add(new ColumnDefinition { ExcelColumn = "A", PropertyName = "Chromosome" });
+            it.ColumnMapping.Add(new ColumnDefinition { ExcelColumn = "B", PropertyName = "Region" });
+            it.ColumnMapping.Add(new ColumnDefinition { ExcelColumn = "C", PropertyName = "Type" });
+            it.ColumnMapping.Add(new ColumnDefinition { ExcelColumn = "F", PropertyName = "Length" });
+            it.ColumnMapping.Add(new ColumnDefinition { ExcelColumn = "G", PropertyName = "Count" });
+            it.ColumnMapping.Add(new ColumnDefinition { ExcelColumn = "H", PropertyName = "Coverage" });
+            return it;
         }
     }
 }
