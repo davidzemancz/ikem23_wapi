800032add5b2e87a7daf0ed33bf1c3baa1fa7d2c
kwimbik <kwimbiks@gmail.com>
2023-11-24 17:17:44 +0100

    read excel

---
+++ ikem23_wapi/XMLReader.cs
@@ -0,0 +1,25 @@
+﻿using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace ikem23_wapi
+{
+    public class XMLReader
+    {
+        public static void ReadEXcelFile(string fileName)
+        {
+            var workbook = new XLWorkbook(fileName);
+            var ws1 = workbook.Worksheet(1);
+
+            var rows = ws1.RangeUsed().RowsUsed().Skip(1); // Skip header row
+            foreach (var row in rows)
+            {
+                var rowNumber = row.RowNumber();
+                foreach (var col in ws1.ColumnsUsed())
+                {
+                    int colNum = col.ColumnNumber();
+                    object value  = row.Cell(colNum).Value;
+                }
+            }
+        }
+    }
+}
