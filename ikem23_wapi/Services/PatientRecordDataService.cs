9f83d8e10619fe9cc31eb892755ee7a5d89f9cef
kwimbik <kwimbiks@gmail.com>
2023-11-25 13:52:03 +0100

    readExcel fce

--- ikem23_wapi/Services/PatientRecordDataService.cs
+++ ikem23_wapi/Services/PatientRecordDataService.cs
@@ -13,7 +13,15 @@ namespace ikem23_wapi.Services
 
         public async Task Post(PatientRecordCreateDto patientRecorDto)
         {
-              
+            DiagnosticReport diagnosticReport = new DiagnosticReport();
+            List<Observation> observations = new List<Observation>();
+            List<MolecularSequence> molecularSequences = new List<MolecularSequence>();
+            
+            foreach (var file in patientRecorDto.Files)
+            {
+                var sequence = ReadMolecularSequence(file, template);
+                molecularSequences.Concat(sequence);
+            }
         }
 
         public async Task Delete(int id)
